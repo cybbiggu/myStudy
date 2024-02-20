05a6e16fa213f4ff96d1d920c29f00e8f846f070
cybbiggu <70556438+cybbiggu@users.noreply.github.com>
2024-02-20 23:08:29 +0900

    Add files via upload

---
+++ A2_Hello_World/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hello World : " + args[0] + " " + args[1]);
+            }
+            else
+            {
+                Console.WriteLine("Hello World : args.Length = 0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
